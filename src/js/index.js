29c920babce8978d771f36b641c06821c61f8b8e
Ziad AlDohaim <ziada2@illinois.edu>
2025-09-08 18:38:37 -0500

    Done with MP0, might add more javascript stuff and minimization needs some tweaking (js)

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,30 @@
 /* Your JS here. */
-console.log('Hello World!')
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("Hello World!");
+  const redbutton = document.querySelector(".red_button");
+  const graybutton = document.querySelector(".gray_button");
+  const yellowbutton = document.querySelector(".yellow_button");
+  const bgColor = document.querySelector(".background");
+  const Image = document.querySelector("img");
+  const fold = document.querySelector(".fold");
+  redbutton.addEventListener("click", () => {
+    bgColor.style.backgroundColor = "red";
+  });
+
+  yellowbutton.addEventListener("click", () => {
+    bgColor.style.backgroundColor = "yellow";
+  });
+
+  graybutton.addEventListener("click", () => {
+    bgColor.style.backgroundColor = "gray";
+  });
+
+  Image.addEventListener("click", () => {
+    //used AI for this:
+    if (fold.style.display === "none") {
+      fold.style.display = "block";
+    } else {
+      fold.style.display = "none";
+    }
+  });
+});
